76a0c95cc0977e2f4be1298a2c81ceb9f1155e59
kansai <kansai.119917@gmail.com>
2022-03-17 23:00:57 +0900

    Initial Commit

---
+++ swiftui-api-client/swiftui_api_clientApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_api_clientApp.swift
+//  swiftui-api-client
+//
+//  Created by kansai okadome on 2022/03/17.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_api_clientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
